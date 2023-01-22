a0bbf96bb1e613dc787bb6a99e1bbf9cdaae6c62
Chris <BOBitisme@gmail.com>
2023-01-22 21:59:33 +1300

    Added character esacaping

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
 const app = express();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'grade_tracker'
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "grade_tracker",
 });
 
 app.use(cors());
@@ -26,81 +26,93 @@ const reply = (res, err, result) => {
     } else {
         res.send(result);
     }
-}
+};
 
 // Hashing function
 const getSaltHash = (password) => {
-    const salt = crypto.randomBytes(16).toString('hex');
+    const salt = crypto.randomBytes(16).toString("hex");
     const hash = getHash(password, salt);
 
     return {
         salt: salt,
-        hash: hash
+        hash: hash,
     };
-}
+};
 
 // Combine salt and password to create hash
 const getHash = (password, salt) => {
-    const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+    const hash = crypto
+        .pbkdf2Sync(password, salt, 1000, 64, "sha512")
+        .toString("hex");
 
     return hash;
-}
-
+};
 
 // ========== Post requests ==========
 
 // Add a new user
-app.post('/api/users', (req, res) => {
-    const email = req.body.email;
+app.post("/api/users", (req, res) => {
+    const email = mysql.escape(req.body.email);
     const password = req.body.password;
-    const name = req.body.name;
+    const name = mysql.escape(req.body.name);
 
     const hashedPassword = getSaltHash(password);
 
-    const sqlInsert = "INSERT INTO users (Email, Password, Salt, DisplayName) VALUES (?, ?, ?, ?)";
-    db.query(sqlInsert, [email, hashedPassword.hash, hashedPassword.salt, name], (err, result) => {
-        if (err) {
-            if (err.code === 'ER_DUP_ENTRY') {
-                res.sendStatus(409);
+    const sqlInsert =
+        "INSERT INTO users (Email, Password, Salt, DisplayName) VALUES (?, ?, ?, ?)";
+    db.query(
+        sqlInsert,
+        [email, hashedPassword.hash, hashedPassword.salt, name],
+        (err, result) => {
+            if (err) {
+                if (err.code === "ER_DUP_ENTRY") {
+                    res.sendStatus(409);
+                } else {
+                    console.log(err);
+                    res.sendStatus(500);
+                }
             } else {
-                console.log(err);
-                res.sendStatus(500);
+                res.sendStatus(200);
             }
-        } else {
-            res.sendStatus(200);
         }
-    });
+    );
 });
- 
+
 // Add a new course
-app.post('/api/courses', (req, res) => {
+app.post("/api/courses", (req, res) => {
     const courseCode = req.body.courseCode;
-    const courseName = req.body.courseName;
+    const courseName = mysql.escape(req.body.courseName);
     const trimester = req.body.trimester;
 
-    const sqlInsert = "INSERT INTO courses (CourseCode, CourseName, TrimesterTaught) VALUES (?, ?, ?)";
+    const sqlInsert =
+        "INSERT INTO courses (CourseCode, CourseName, TrimesterTaught) VALUES (?, ?, ?)";
     db.query(sqlInsert, [courseCode, courseName, trimester], (err, result) => {
         reply(res, err, result);
     });
 });
 
 // Add a new assignment
-app.post('/api/assignments', (req, res) => {
+app.post("/api/assignments", (req, res) => {
     const courseCode = req.body.courseCode;
     const trimester = req.body.trimester;
     const assignmentName = req.body.assignmentName;
     const weight = req.body.weight;
     const dueDate = req.body.dueDate;
 
-    const sqlInsert = "INSERT INTO assignments (CourseCode, Trimester, AssignmentName, Weight, DueDate) VALUES (?, ?, ?, ?, ?)";
-    db.query(sqlInsert, [courseCode, trimester, assignmentName, weight, dueDate], (err, result) => {
-        reply(res, err, result);
-    });
+    const sqlInsert =
+        "INSERT INTO assignments (CourseCode, Trimester, AssignmentName, Weight, DueDate) VALUES (?, ?, ?, ?, ?)";
+    db.query(
+        sqlInsert,
+        [courseCode, trimester, assignmentName, weight, dueDate],
+        (err, result) => {
+            reply(res, err, result);
+        }
+    );
 });
 
 // Authenticate a user
-app.post('/api/authorise', (req, res) => {
-    const email = req.body.email;
+app.post("/api/authorise", (req, res) => {
+    const email = mysql.escape(req.body.email);
     const password = req.body.password;
 
     const sqlSelect = "SELECT * FROM users WHERE Email = ?";
@@ -114,7 +126,7 @@ app.post('/api/authorise', (req, res) => {
                 if (hash === result[0].Password) {
                     res.send({
                         email: result[0].Email,
-                        displayName: result[0].DisplayName
+                        displayName: result[0].DisplayName,
                     });
                 } else {
                     res.sendStatus(401);
@@ -127,7 +139,7 @@ app.post('/api/authorise', (req, res) => {
 });
 
 // Add a course to a user
-app.post('/api/user/courses', (req, res) => {
+app.post("/api/user/courses", (req, res) => {
     const userID = req.body.userID;
     const courseCode = req.body.courseCode;
     const year = req.body.year;
@@ -135,17 +147,21 @@ app.post('/api/user/courses', (req, res) => {
     const grades = req.body.grades;
     const totalGrade = req.body.totalGrade;
 
-    const sqlInsert = "INSERT INTO grades (Email, CourseCode, Year, Trimester, Grades, TotalGrade) VALUES (?, ?, ?, ?, ?, ?)";
-    db.query(sqlInsert, [userID, courseCode, year, trimester, grades, totalGrade], (err, result) => {
-        reply(res, err, result);
-    });
+    const sqlInsert =
+        "INSERT INTO grades (Email, CourseCode, Year, Trimester, Grades, TotalGrade) VALUES (?, ?, ?, ?, ?, ?)";
+    db.query(
+        sqlInsert,
+        [userID, courseCode, year, trimester, grades, totalGrade],
+        (err, result) => {
+            reply(res, err, result);
+        }
+    );
 });
-
 
 // ========== Get requests ==========
 
 // Get all course with filter by tri option
-app.get('/api/courses', (req, res) => {
+app.get("/api/courses", (req, res) => {
     const trimester = req.query.trimester;
     if (trimester == null) {
         const sqlSelect = "SELECT * FROM courses";
@@ -161,9 +177,9 @@ app.get('/api/courses', (req, res) => {
 });
 
 // Get user courses with optional filter of year
-app.get('/api/user/courses', (req, res) => {
+app.get("/api/user/courses", (req, res) => {
     const userId = req.query.userId;
-    const year = req.query.year; 
+    const year = req.query.year;
 
     if (year == null) {
         const sqlSelect = "SELECT * FROM grades WHERE Email = ?";
@@ -179,11 +195,12 @@ app.get('/api/user/courses', (req, res) => {
 });
 
 // Get all assignments for a course
-app.get('/api/:course/assignments', (req, res) => {
+app.get("/api/:course/assignments", (req, res) => {
     const courseCode = req.params.course;
     const trimester = req.query.trimester;
 
-    const sqlSelect = "SELECT * FROM assignments WHERE CourseCode = ? AND Trimester = ?";
+    const sqlSelect =
+        "SELECT * FROM assignments WHERE CourseCode = ? AND Trimester = ?";
     db.query(sqlSelect, [courseCode, trimester], (err, result) => {
         reply(res, err, result);
     });
@@ -192,33 +209,38 @@ app.get('/api/:course/assignments', (req, res) => {
 // ========== Put requests ==========
 
 // Update user details - Could be a better way to do this
-app.put('/api/user', (req, res) => {
+app.put("/api/user", (req, res) => {
     const userId = req.body.userId;
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
-    
-    const sqlUpdate = "UPDATE users SET Email = ?, Password = ?, DisplayName = ? WHERE Email = ?";
+
+    const sqlUpdate =
+        "UPDATE users SET Email = ?, Password = ?, DisplayName = ? WHERE Email = ?";
     db.query(sqlUpdate, [email, password, name, userId], (err, result) => {
         reply(res, err, result);
     });
 });
 
 // Update a user's grades
-app.put('/api/courses/', (req, res) => {
+app.put("/api/courses/", (req, res) => {
     const userId = req.body.userId;
     const courseCode = req.body.courseCode;
     const year = req.body.year;
     const trimester = req.body.trimester;
     const grades = req.body.grades;
 
-    const sqlUpdate = "UPDATE grades SET Grades = ? WHERE Email = ? AND CourseCode = ? AND Year = ? AND Trimester = ?";
-    db.query(sqlUpdate, [grades, userId, courseCode, year, trimester], (err, result) => {
-        reply(res, err, result);
-    });
+    const sqlUpdate =
+        "UPDATE grades SET Grades = ? WHERE Email = ? AND CourseCode = ? AND Year = ? AND Trimester = ?";
+    db.query(
+        sqlUpdate,
+        [grades, userId, courseCode, year, trimester],
+        (err, result) => {
+            reply(res, err, result);
+        }
+    );
 });
 
-
 app.listen(PORT, () => {
-    console.log('Example app listening on port ' + PORT);
+    console.log("Example app listening on port " + PORT);
 });
