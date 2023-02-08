60cf37aeb39be4f1ef6bd35d3d994e6a75dc31fb
Chris <BOBitisme@gmail.com>
2023-02-09 00:47:05 +1300

    Updated success colors

--- client/src/Themes.js
+++ client/src/Themes.js
@@ -14,7 +14,7 @@ const lightTheme = createTheme({
             main: "rgba(215, 64, 0, .8)",
         },
         success: {
-            main: "rgba(50, 150, 10, .8)",
+            main: "#1b440b",
         },
         error: {
             main: "rgb(200, 0, 0)",
@@ -34,7 +34,7 @@ const lightTheme = createTheme({
                     color: "#000000",
                 },
                 standardSuccess: {
-                    backgroundColor: "rgba(0, 255, 0, 0.3)",
+                    backgroundColor: "rgba(50, 150, 10, .5)",
                     color: "#000000",
                 },
                 standardWarning: {
@@ -54,6 +54,10 @@ const lightTheme = createTheme({
             styleOverrides: {
                 colorPrimary: {
                     backgroundColor: "rgba(100, 100, 100, 0.8)",
+                    color: "#ffffff",
+                },
+                colorSuccess: {
+                    backgroundColor: "rgba(50, 150, 10, .8)",
                     color: "#ffffff",
                 },
             },
