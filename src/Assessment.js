8e4f85dd09be2cd492e764066d8bab826f79507e
AboodAsfari <abdz2004@gmail.com>
2023-02-25 12:47:46 +1300

    Created unified data structures for courses and assignments

---
+++ src/Assessment.js
@@ -0,0 +1,73 @@
+class Assessment {
+    constructor(name, weight, grade, deadline, isAss, isNew) {
+        this.name = name;
+        this.weight = weight;
+        this.grade = grade === -1 ? NaN : grade;
+        this.deadline = deadline;
+
+        this.gradeValid = true;
+        this.valid = true;
+        this.isNew = isNew;
+
+        this.duplicateName = false;
+
+        this.isAss = isAss;
+        this.hasChanged = isNew;
+        this.stopTransition = false;
+
+        this.initName = name;
+        this.initDeadline = deadline;
+        this.initGrade = grade === -1 ? NaN : grade;
+        this.initAss = isAss;
+        this.initWeight = weight;
+    }
+
+    checkValid() {
+        let nameValid = !(this.name.length === 0 || this.name.length > 30 || this.duplicateName);
+        let weightValid = this.weight > 0 && this.weight <= 100;
+        this.valid = nameValid && this.gradeValid && weightValid;
+    }
+
+    checkIfChanged() {
+        let gradeChanged = isNaN(this.grade) ? !isNaN(this.initGrade) : this.grade !== this.initGrade;
+        let nameChanged = this.name !== this.initName;
+        let deadlineChanged = this.deadline !== this.initDeadline;
+        let isAssChanged = this.isAss !== this.initAss;
+        let weightChanged = this.weight !== this.initWeight;
+        this.hasChanged = this.isNew || gradeChanged || nameChanged || deadlineChanged || isAssChanged || weightChanged;
+    }
+
+    setGrade(grade) {
+        this.grade = grade;
+        this.checkIfChanged();
+    }
+
+    setName(name) {
+        this.name = name;
+        this.checkIfChanged();
+        this.checkValid();
+    }
+
+    setDeadline(deadline) {
+        this.deadline = deadline;
+        this.checkIfChanged();
+    }
+
+    setWeight(weight){
+        this.weight = weight;
+        this.checkValid();
+        this.checkIfChanged();
+    }
+
+    setIsAss(isAss) {
+        this.isAss = isAss;
+        this.checkIfChanged();
+    }
+
+    equalsTemplate(template) {
+        return this.name === template.name && this.deadline === template.deadline && 
+            this.isAss === template.isAss && this.weight.toString() === template.weight.toString();
+    }
+} 
+
+export default Assessment;
---
+++ src/Course.js
@@ -0,0 +1,47 @@
+class Course {
+    constructor(code, name, url, assessments, totalGrade, lastUpdated, lastSynced, tri, year) {
+        this.code = code;
+        this.name = name;
+        this.assessments = assessments;
+        this.totalGrade = totalGrade;
+        this.tri = tri;
+        this.year = year;
+        this.url = url;
+        this.lastUpdated = new Date(lastUpdated);
+        this.lastSynced = new Date(lastSynced);
+    }
+
+    getCourseCompletion() {
+        let finished = 0;
+        this.assessments.forEach((ass) => {
+            if (ass.grade !== -1 && !isNaN(ass.grade)) finished++;
+        });
+        return finished / this.grades.length;
+    }
+
+    getCourseLetter() {
+        if(isNaN(this.totalGrade)) return "N/A";
+        else if(this.totalGrade >= 90) return "A+";
+        else if(this.totalGrade >= 85) return "A";
+        else if(this.totalGrade >= 80) return "A-";
+        else if(this.totalGrade >= 75) return "B+";
+        else if(this.totalGrade >= 70) return "B";
+        else if(this.totalGrade >= 65) return "B-";
+        else if(this.totalGrade >= 60) return "C+";
+        else if(this.totalGrade >= 55) return "C";
+        else if(this.totalGrade >= 50) return "C-";
+        else if(this.totalGrade >= 40) return "D";
+        return "E";
+    }
+
+    updateTotal() {
+        let temp = 0;
+        this.assessments.forEach((ass) => {
+            let num = isNaN(ass.grade) ? 0 : ass.grade;
+            temp += (num * ass.grade.weight * 0.01);
+        });
+        this.totalGrade = temp.toFixed(2);
+    }
+}
+
+export default Course;
