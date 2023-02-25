d25d9962a832a3bbcb9b30ce8eebf29a309c222b
AboodAsfari <abdz2004@gmail.com>
2023-02-25 13:06:51 +1300

    Added base course viewer functionality with new data structure

--- src/CourseViewer.jsx
+++ src/CourseViewer.jsx
@@ -68,78 +68,7 @@ import FilterListIcon from "@mui/icons-material/FilterList";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import LaunchIcon from "@mui/icons-material/Launch";
 import MenuBookRoundedIcon from "@mui/icons-material/MenuBookRounded";
-
-class Assessment {
-    constructor(name, weight, grade, deadline, isAss, isNew) {
-        this.name = name;
-        this.weight = weight;
-        this.grade = grade === -1 ? NaN : grade;
-        this.deadline = deadline;
-
-        this.gradeValid = true;
-        this.valid = true;
-        this.isNew = isNew;
-
-        this.duplicateName = false;
-
-        this.isAss = isAss;
-        this.hasChanged = isNew;
-        this.stopTransition = false;
-
-        this.initName = name;
-        this.initDeadline = deadline;
-        this.initGrade = grade === -1 ? NaN : grade;
-        this.initAss = isAss;
-        this.initWeight = weight;
-    }
-
-    checkValid() {
-        let nameValid = !(this.name.length === 0 || this.name.length > 30 || this.duplicateName);
-        let weightValid = this.weight > 0 && this.weight <= 100;
-        this.valid = nameValid && this.gradeValid && weightValid;
-    }
-
-    checkIfChanged() {
-        let gradeChanged = isNaN(this.grade) ? !isNaN(this.initGrade) : this.grade !== this.initGrade;
-        let nameChanged = this.name !== this.initName;
-        let deadlineChanged = this.deadline !== this.initDeadline;
-        let isAssChanged = this.isAss !== this.initAss;
-        let weightChanged = this.weight !== this.initWeight;
-        this.hasChanged = this.isNew || gradeChanged || nameChanged || deadlineChanged || isAssChanged || weightChanged;
-    }
-
-    setGrade(grade) {
-        this.grade = grade;
-        this.checkIfChanged();
-    }
-
-    setName(name) {
-        this.name = name;
-        this.checkIfChanged();
-        this.checkValid();
-    }
-
-    setDeadline(deadline) {
-        this.deadline = deadline;
-        this.checkIfChanged();
-    }
-
-    setWeight(weight){
-        this.weight = weight;
-        this.checkValid();
-        this.checkIfChanged();
-    }
-
-    setIsAss(isAss) {
-        this.isAss = isAss;
-        this.checkIfChanged();
-    }
-
-    equalsTemplate(template) {
-        return this.name === template.name && this.deadline === template.deadline && 
-            this.isAss === template.isAss && this.weight.toString() === template.weight.toString();
-    }
-} 
+import Assessment from "./Assessment";
 
 const CourseViewer = (props) => {
     const { courseData, setViewedCourse, userDetails, setSessionData, sessionData, setCourseList } = props;
@@ -224,16 +153,10 @@ const CourseViewer = (props) => {
         setCourseCompletion((courseData.getCourseCompletion() * 100).toFixed(2));
         setCourseLetter(courseData.getCourseLetter());
 
-        for(let i = 0; i < courseData.names.length; i++){
-            const name = courseData.names[i];
-            const weight = courseData.weights[i];
-            const deadline = courseData.deadlines[i];
-            const grade = courseData.grades[i];
-            const isAss = courseData.isAssList[i];
-            const assessment = new Assessment(name, weight, grade, deadline, isAss);
-            setAssessments(current => [...current, assessment]);
-            setFilteredAssessments(current => [...current, assessment]);
-        };
+        courseData.assessments.forEach((assessment) => {
+            setAssessments(current => [...current, assessment.clone()]);
+            setFilteredAssessments(current => [...current, assessment.clone()]);
+        })
     }, [assessments.length, courseData, handleKeyDown, handleTransitionEnd, handleTransitionStart]);
 
     const sort = useCallback((type = sortType, list = filteredAssessments) => {
@@ -855,4 +778,3 @@ const CourseViewer = (props) => {
 }
 
 export default CourseViewer;
-export { Assessment };
