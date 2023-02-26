473d7855447354ea819e8146700030028675aeb7
Chris <BOBitisme@gmail.com>
2023-02-27 02:03:59 +1300

    Emails are now sent from different addresses

--- lambdas/handleFeedbackSubmission..js
+++ lambdas/handleFeedbackSubmission..js
@@ -20,10 +20,6 @@ import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 import FeedbackEmailTemplate from "./FeedbackEmailTemplate.mjs";
 
 const sesClient = new SESClient({ region: "ap-southeast-2" });
-const source = `2359 Admin <no-reply@twentythreefiftynine.com>`;
-const bugEmail = "2359gradetracker+bugs@gmail.com";
-const suggestionEmail = "2359gradetracker+suggestions@gmail.com";
-const supportEmail = "2359gradetracker+support@gmail.com";
 
 export const handler = async(event) => {
     if(event.requestContext.http.method !== "POST") {
@@ -37,7 +33,7 @@ export const handler = async(event) => {
     
     const params = {
         Destination: {
-            ToAddresses: [feedbackType === "bug" ? bugEmail : feedbackType === "suggestion" ? suggestionEmail : supportEmail],
+            ToAddresses: [`2359gradetracker+${feedbackType}@gmail.com`],
         },
         Message: {
             Body: {
@@ -56,7 +52,7 @@ export const handler = async(event) => {
                 Data: subject,
             },
         },
-        Source: source,
+        Source: `${toTitleCase(feedbackType)} 2359 <${feedbackType}@twentythreefiftynine.com>`,
     };
 
     const command = new SendEmailCommand(params);
@@ -84,7 +80,13 @@ export const handler = async(event) => {
     
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Success'),
     };
     return response;
+};
+
+const toTitleCase = (str) => {
+    return str.replace(/\w\S*/g, (txt) => {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 };
