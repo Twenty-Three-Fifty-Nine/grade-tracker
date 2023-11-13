e2ea1208a8f2e4079532f4c4f4b3f2953a86cd31
Chris <BOBitisme@gmail.com>
2023-11-13 17:23:04 +1300

    Updated current trimester

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ const App = () => {
 
     // Tells both the client and the API what the currently active trimester is.
     const activeTri = useMemo(() => {
-        return { year: 2023, tri: 2 };
+        return { year: 2023, tri: 3 };
     }, []);
 
     // States related to the year select tabs.
